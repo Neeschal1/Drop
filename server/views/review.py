f020a5dba28adbba080add3071c219a6a5edd3f2
Neeschal1 <neeschalpok04@gmail.com>
2026-09-14 11:42:38 +0545

    ft: implementation of fetching all user reviews

--- server/views/review.py
+++ server/views/review.py
@@ -1,10 +1,12 @@
 from schemas.review import Review as ReviewSchema
 from models.setup import Review as ReviewDB
 from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse
 from database.dependencies import db_dependencies
 
 reviewrouter = APIRouter()
 
+# Create a new customer review
 @reviewrouter.post('/review/post/', status_code=status.HTTP_201_CREATED, tags=["Review"])
 async def create_review(review: ReviewSchema, db: db_dependencies):
     try:
@@ -17,3 +19,20 @@ async def create_review(review: ReviewSchema, db: db_dependencies):
     except Exception as e:
         db.rollback()
         return {"message": "Exception occured!", "detail": str(e)}
+
+
+# List all customer reviews
+@reviewrouter.get('/review/list-all-review/', status_code=status.HTTP_200_OK, tags=["Review"])
+async def list_all_review(db: db_dependencies):
+    try:
+        db_review = db.query(ReviewDB).all()
+        if not db_review:
+            return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={
+                "message": "No customer review available!"
+            })
+        return {"message": "Customer reviews listed successfully :)", "data": db_review}
+    except Exception as e:
+        db.rollback()
+        return {"message": "Exception occured!", "detail": str(e)}
+    
+    
