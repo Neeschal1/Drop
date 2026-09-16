b12fdfea5bd580713d75e6a5d8c1ede766cfd525
Neeschal1 <neeschalpok04@gmail.com>
2026-09-16 13:13:21 +0545

    md: connecting deployed server url with the existing client side architecture

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
-
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
-});
+}
+);
 
 // Request interceptor: attach Bearer token if available
 api.interceptors.request.use(
